03729153ce9aa34764905e3d14e53a9bfb235d4c
Osman Ünüvar <osmanunuvar@gmail.com>
2022-03-13 16:41:18 +0300

    comment model updated

---
+++ DataAccessLayer/Migrations/202203131337285_comment_date_added.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class comment_date_added : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Comments", "CommentDate", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Comments", "CommentDate");
+        }
+    }
+}
